43c470fc2f6c17f82bb4ae0c2449be07782512e5
Dan Longo <danlongo19@gmail.com>
2019-07-21 12:27:51 -0500

    update

--- server.js
+++ server.js
@@ -34,7 +34,8 @@ app.engine("handlebars", expressHandlebars({
 }));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/onionHeadlines", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 
